94058f7d8e4b2791fdfcda7de5759775721839ee
nabendu82 <nabendu.biswas@gmail.com>
2019-06-21 21:45:04 +0530

    Server Side GraphQL Done

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const mutations = {};
+const Mutations = {
+    async createItem(parent, args, ctx, info) {
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+        return item;
+    }
+};
 
-module.exports = mutations;
+module.exports = Mutations;
